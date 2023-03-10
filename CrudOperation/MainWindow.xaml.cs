32427afd8a92cea5198af7281bebb556b1039bf0
Md-Emrul-Hasan-Emon <ehe3025@bnxnetworks.com>
2023-03-11 02:50:35 +0600

    Database Connection

--- CrudOperation/MainWindow.xaml.cs
+++ CrudOperation/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace CrudOperation
 {
@@ -23,7 +25,9 @@ namespace CrudOperation
         public MainWindow()
         {
             InitializeComponent();
+            LoadGrid();
         }
+        SqlConnection con = new SqlConnection(@"Data Source=.\sqlexpress;Initial Catalog=CrudWpf;Integrated Security=True");
 
         public void clearData()
         {
@@ -32,10 +36,53 @@ namespace CrudOperation
             gender_txt.Clear();
             city_txt.Clear();
         }
+        public void LoadGrid()
+        {
+            SqlCommand cmd = new SqlCommand("select * from FirstTable", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+        }
 
         private void ClearDataBtn_Click(object sender, RoutedEventArgs e)
         {
             clearData();
+        }
+        public bool isValid()
+        {
+            if( name_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Name is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (age_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Age is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (gender_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Gendedr is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (city_txt.Text == string.Empty)
+            {
+                MessageBox.Show("City is required", "Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void InsertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (isValid())
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO FirstTable VALUES (@Name,@Age,@Gender,@City)",con);
+                cmd.CommandType = CommandType.Text();
+            }
         }
     }
 }
